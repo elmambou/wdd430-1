094ca43429c4a8340e9079134df3c9558e5cb6b7
Samuel Bradshaw <samuel.h.bradshaw@gmail.com>
2021-05-15 10:50:25 -0600

    Week 04 assignment

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
@@ -7,11 +7,18 @@ import { Document } from '../document.model';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent: EventEmitter<Document> = new EventEmitter<Document>();
 
   documents: Document[] = [
-    new Document(1, 'My Document', 'This is my document!', 'https://www.google.com/', []),
-    new Document(2, 'Another Document', 'This is my other document!', 'https://www.bing.com/', []),
+    new Document(1, 'Document One', 'This is my document!', 'https://www.google.com/', []),
+    new Document(2, 'Document Two', 'This is my other document!', 'https://www.bing.com/', []),
+    new Document(3, 'Document Three', 'I am the third document!', 'https://duckduckgo.com/', []),
+    new Document(4, 'Document Four', 'I am the fourth document!', 'https://singpraises.net/', []),
   ];
+
+  onSelected(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
   constructor() { }
 
